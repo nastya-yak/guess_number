b4285aea02fe1357e490f1e6da2abe089ab7af71
Nastya Yakovenko <betelgayse@gmail.com>
2024-01-18 15:20:25 +0300

    linter fix

--- guess_number.py
+++ guess_number.py
@@ -6,14 +6,6 @@ from random import randint
 number = randint(1, 100)
 print('Угадайте число от 1 до 100')
 
-# Импорт функции получения случайных чисел
-# из модуля random.
-from random import randint
-
-# Получаем случайное число в диапазоне от 1 до 100.
-number = randint(1, 100)
-print('Угадайте число от 1 до 100')
-
 while True:
     # Получаем число от пользователя и сохраняем его в переменную.
     guess = int(input('Введите число: '))
